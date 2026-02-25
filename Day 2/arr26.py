5e23835336f92448f7faf2947643c29de3e8518a
= <=>
2026-02-25 10:57:33 +0530

    Add Day 2

---
+++ Day 2/arr26.py
@@ -0,0 +1,32 @@
+#BRUTE FORCE SOLUTION
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        n = len(nums)
+        freq_map = {}
+
+        for i in range(n):
+            freq_map[nums[i]] = 1
+
+        j = 0
+
+        for key in freq_map:
+            nums[j] = key
+            j += 1
+
+        return j
+#OPTIMAL SOLUTION
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        n = len(nums)
+
+        if n == 0:
+            return 0
+
+        i = 0  # slow pointer
+
+        for j in range(1, n):  # fast pointer
+            if nums[j] != nums[i]:
+                i += 1
+                nums[i] = nums[j]
+
+        return i + 1

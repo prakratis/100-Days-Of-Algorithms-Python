6c694d34a325e0eeeddfe2fbccece2d3137e671a
= <=>
2026-03-11 22:39:48 +0530

    Adding Solutions

---
+++ Day 6/arr136.py
@@ -0,0 +1,9 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        result = 0
+        n = len(nums)
+
+        for i in range (0 , n):
+             result ^= nums[i]
+        
+        return result
